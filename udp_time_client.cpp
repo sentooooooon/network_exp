5b76b6609347e9da3e6bee183b9519335113f6bd
sentooooooon <133179758+sentooooooon@users.noreply.github.com>
2024-01-24 17:30:39 +0900

    Add files via upload

--- udp_time_client.cpp
+++ udp_time_client.cpp
@@ -33,8 +33,8 @@ int main(int argc, char* argv[])
     in_port_t port_num = 5000; // ポート番号
     int n = 0; // 戻り値の保存用
     char buff[BUFF_SIZE]; // 送受信用バッファ
-    int c = 0;//
-    int i = 0;
+    //
+    
 
     if (argc == 2) {
         serv_ip = argv[1];
@@ -58,36 +58,64 @@ int main(int argc, char* argv[])
     mt19937_64 mt(rd());
     uniform_int_distribution<char> cU(30, 80);
     int loop;
-    cin >> loop;
+    //cin >> loop;
     int power = pow(2,16);
     int count = 0;
-    for(int j = 0; j < loop; j++)
-    { string  msg;
-      auto start = system_clock::now(); 
-    while(count < power){
-	    count++;
-	    char ch = cU(mt);
-	    buff[c] = ch;
-	    if(c == BUFF_SIZE){
-		    n = sendto(socketd, buff, BUFF_SIZE, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-		    memset(buff, 0, sizeof(buff));
-		    c == 0;}
-	    else{
-	     c++;}
-    }
+      string  msg;
+      auto start = system_clock::now();
+     //generate random message 
+      for(int i = 0; i < power; i++){
+	      char c = cU(mt);
+	      msg += c;
+      
+      }
+      //cout << "made random message" << endl;
+      cout << msg << endl;
+      cout << "------------------------------------------------" << endl;
+	int c = 0; 
+	int j = 0;
+
+	
+     while(true){
+	     if(j == msg.size()){
+		     c++;
+		     buff[c] = '\0';
+		     n = sendto(socketd, buff, sizeof(buff), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+		     memset(buff, 0, sizeof(buff));
+		     //cout << "break" << endl;
+		     break;
+	     }
+	     else if(c == BUFF_SIZE - 1){
+		     buff[c] == '\0';
+		     n = sendto(socketd, buff, sizeof(buff), 0 , (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+		     c = 0;
+		     //cout << "sent" << endl;
+		     cout << buff << endl;
+		     memset(buff, 0 ,sizeof(buff));
+	     }
+	     //cout << j << endl;
+	     buff[c] = msg[j]; j++; c++;
+     }
+     buff[0] = '~';
+     buff[1] = 0;
+     n = sendto(socketd, buff, sizeof(buff), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+     //cout << "loop deta" << endl;
 
     //if(msg == "END")break;
-    //n = sendto(socketd, msg.c_str(), msg.size(), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    //n = sendto(socketd, buff, BUFF_SIZE, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     if (n < 0) {
         cout << "failed to receive a message.\n";
         return -1;
     }
     // サーバから現在時刻を文字列として受信．
+    cout << "befor recieve" << endl;
     n = recvfrom(socketd, buff, sizeof(buff)-1, 0, NULL, NULL); // 終端文字列を入れるために，sizeof(buff)-1 として，文字列一つ分必ず余裕を持たせてデータを受信する．buff をこのまま文字列として使わない場合は全記憶を受信に使う．
     if (n < 0) {
         cout << "Failed to receive a message.\n";
         return -1;
     }
+    //cout << "after recieve" << endl;
     auto end = system_clock::now();
 
     duration<double> elapsed_seconds = end - start;
@@ -96,7 +124,6 @@ int main(int argc, char* argv[])
     cout << "Time: " << elapsed_seconds.count() << " s, " << htons(serv_addr.sin_port)<< "\n";
     memset(buff, 0, sizeof(buff));
 	//cout << "end" <<endl;
-    }
     // ソケットを閉じる
     close(socketd);
 }
